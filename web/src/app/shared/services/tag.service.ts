ce6f3f0c1ac1945cf695774469207c1bf7b080ba
alessfm <alessandrofm@aluno.fapce.edu.br>
2025-02-19 21:58:35 -0300

    wip: Buscar tags em destaque (maior uso)

--- web/src/app/shared/services/tag.service.ts
+++ web/src/app/shared/services/tag.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs';
 import { GenericService } from '@core/services/generic.service';
+
+import { Loading } from '@core/models/loading.model';
 import { Tag } from '../models/tag.model';
 
 @Injectable()
@@ -9,5 +12,12 @@ export class TagService extends GenericService<Tag> {
 
   constructor(http: HttpClient) {
     super('api/tags', http)
+  }
+
+  buscarDestaques(loading?: Loading): Observable<Tag[]> {
+    this.startLoading(loading);
+    return this.getHttpClient()
+      .get<Tag[]>(`${this.api}/destaque`)
+      .pipe(this.configMapAndLoading(loading));
   }
 }
