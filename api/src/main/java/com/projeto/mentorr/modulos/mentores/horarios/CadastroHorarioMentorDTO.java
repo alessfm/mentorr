906bfd8ab774ff9d4803b586a7a9ba51d37ea845
alessfm <alessandrofm@aluno.fapce.edu.br>
2025-09-30 14:48:10 -0300

    Reajustar DTOs e separar rotas dos horários

--- api/src/main/java/com/projeto/mentorr/modulos/mentores/horarios/CadastroHorarioMentorDTO.java
+++ api/src/main/java/com/projeto/mentorr/modulos/mentores/horarios/CadastroHorarioMentorDTO.java
@@ -4,23 +4,17 @@ import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@Builder
 @Data
-@NoArgsConstructor
 public class CadastroHorarioMentorDTO {
 
 	@NotNull(message = "O dia de funcionamento é obrigatório")
 	private DayOfWeek dia;
-	
+
 	@NotNull(message = "O horário inicial é obrigatório")
 	private LocalTime horaInicio;
-	
+
 	@NotNull(message = "O horário final é obrigatório")
 	private LocalTime horaFim;
 
--- api/src/main/java/com/projeto/mentorr/modulos/mentores/horarios/HorarioMentorDTO.java
+++ api/src/main/java/com/projeto/mentorr/modulos/mentores/horarios/HorarioMentorDTO.java
@@ -6,30 +6,31 @@ import java.time.LocalTime;
 import com.projeto.mentorr.util.Util;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@Builder
 @Data
 @NoArgsConstructor
 public class HorarioMentorDTO {
 
 	private Long id;
-	private String dia;
+	private DayOfWeek dia;
+	private String descricaoDia;
 	private LocalTime horaInicio;
 	private LocalTime horaFim;
-	
+
 	public HorarioMentorDTO(Long id, DayOfWeek dia, LocalTime horaInicio, LocalTime horaFim) {
 		this.id = id;
-		this.dia = Util.retornarDiaEnumFormatado(dia);
+		this.dia = dia;
+		this.descricaoDia = Util.retornarDiaEnumFormatado(dia);
 		this.horaInicio = horaInicio;
 		this.horaFim = horaFim;
 	}
-	
+
 	public HorarioMentorDTO(DayOfWeek dia, LocalTime horaInicio, LocalTime horaFim) {
-		this.dia = Util.retornarDiaEnumFormatado(dia);
+		this.dia = dia;
+		this.descricaoDia = Util.retornarDiaEnumFormatado(dia);
 		this.horaInicio = horaInicio;
 		this.horaFim = horaFim;
 	}
